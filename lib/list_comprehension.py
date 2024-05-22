5a69ed18fc2e3fa75d52ebea06908c37d41cccd9
Robert Gathanua <robertgathanua@gamail.com>
2024-05-22 06:57:36 +0300

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,9 @@
 
 def return_evens(num_list):
     pass
+    return [x for x in num_list if x % 2 == 0]
+
 
 def make_exclamation(sentence_list):
     pass
+    return [sentence + '!' for sentence in sentence_list]
